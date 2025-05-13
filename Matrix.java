93884f9a4e99c26c51703c03982befb4676fe21a
Keerthana H <keerthanadas247@gmail.com>
2025-05-13 14:27:51 +0530

    Matrix test

---
+++ Matrix.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.Scanner;
+public class Matrix {
+
+	public static void main(String[] args) {
+	Scanner num=new Scanner(System.in);
+	System.out.println("Enter number of rows and columns");
+	int a=num.nextInt();
+	int b=num.nextInt();
+	int[][] matrix=new int[a][b];
+	
+	for(int i=0;i<a;i++) {
+		for(int j=0;j<b;j++) {
+			System.out.println("Enter ");
+		}
+	}
+	}
+
+}
