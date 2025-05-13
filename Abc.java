f5bf006397c1f9cf8cfdca08cd893b5e3aff3142
Keerthana H <keerthanadas247@gmail.com>
2025-05-13 14:24:07 +0530

    Test  message

---
+++ Abc.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		String k="Banglore Tumkur Banglore Tumkur";
+		
+		for(int i=k.length()-1;i>=0;i--)
+		{
+			System.out.print(k.charAt(i));
+		}
+		}
+
+}
